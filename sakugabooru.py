b9c85a9a2b8b69b8fd7c76b9e42cd74deee2e2c7
miishin <miishing@gmail.com>
2023-01-14 23:14:41 -0500

    cleaned up get_most_popular to ignore null inputs and shortened to a one liner

--- sakugabooru.py
+++ sakugabooru.py
@@ -11,11 +11,9 @@ def sakuget(num_posts, tag):
     return r.json()
 
 # sorts the json response
-def sort_posts(posts):
-    return sorted(posts, key=lambda p: p['score'], reverse=True)
+def sort_posts(posts, key, desc):
+    return sorted(posts, key=lambda p: p[key], reverse=desc)
 
-
-sort_posts(sakuget(1, "One_Piece"))
 # grabs the links to the mp4
 def get_mp4_links(r_json):
     links = []
@@ -24,21 +22,14 @@ def get_mp4_links(r_json):
     return links
 
 def get_most_popular(r_json):
-    hs = -1
-    best = None
-    for json_obj in r_json:
-        if json_obj['score'] > hs:
-            hs = json_obj['score']
-            best = json_obj
-    return best
-
+    if not r_json:
+        return None
+    return max(r_json, key=lambda j: j['score']) 
     
 def sakuget_popular_mp4_link(num_posts : int, search_tag : string):
     clip = get_most_popular(sakuget(num_posts, search_tag))
     if clip:
         return clip['file_url']
-    return clip
-
-
+    return None
 
     

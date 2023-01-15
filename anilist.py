dc0e4a70fbdf8c23db10228f802a3128cbbfbbea
miishin <miishing@gmail.com>
2023-01-14 23:05:37 -0500

    improved the clean response function, now removes unwanted characters and replaces them with underscores when they are needed, and otherwise just deletes them. removes trailing underscores too since that is also an edge issue. some titles cannot be fixed with regex because the title is in the wrong order

--- anilist.py
+++ anilist.py
@@ -3,25 +3,24 @@ import requests, re
 ANILIST_URL = 'https://graphql.anilist.co'
 
 # Sakugabooru likes: ! - ; 
-GOOD_CHARACTERS = r'[!-;\']'
-# doesn't like .
-BAD_CHARACTERS = r'[.]'
+CHARS_TO_REPLACE = r'[^a-zA-Z0-9:;.!_]'
+CHARS_TO_REPLACE_WITH_UNDERSCORE = r'_?[^a-zA-Z0-9:;.!_]_?'
 
 # querys for the first 10 favorite anime titles of a given user
-def get_user(username):
+def get_user_favorites(username):
     variables = {'name': username}
     query = '''
     query ($name : String) {
           User(name: $name) {
             favourites{
-                anime(page:1, perPage: 10) {
+                anime(page:1, perPage: 20) {
                     nodes{
                         title {
                             romaji
                         }}}}}}
     '''
     result = requests.post(ANILIST_URL, json={'query': query, 'variables': variables}).json()
-    return result
+    return clean_response(result)
 
 def get_all_anime(username):
     variables = {'name': username}
@@ -35,16 +34,15 @@ def get_all_anime(username):
                         mediaIds}}}}}
     '''
     result = requests.post(ANILIST_URL, json={'query': query, 'variables': variables}).json()
-    return result
+    return clean_response(result)
 
 def clean_response(res):
     if "errors" in res:
         return None
     titles = res["data"]["User"]["favourites"]["anime"]["nodes"]
     for i, title in enumerate(titles):
-        s = re.sub()
-        titles[i] = re.sub(r'\W+', '', title["title"]["romaji"].replace(' ', '_'))
+        titles[i] = title["title"]["romaji"].replace(' ', '_')
+        titles[i] = re.sub(CHARS_TO_REPLACE_WITH_UNDERSCORE, '_', titles[i])
+        titles[i] = re.sub(CHARS_TO_REPLACE, '', titles[i])
+        titles[i] = re.sub(r'_\Z', '', titles[i])
     return titles
-
-
-

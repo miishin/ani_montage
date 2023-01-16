0fac6632ead9ccf441914af854295a5f166fa0b9
miishin <miishing@gmail.com>
2023-01-15 23:16:21 -0500

    started vid merging, seems to be issues with ffmpeg merging the files becasue theyre all just random .mp4s (some are actually gifs) with all different encoding and dimensions etc

--- vidmerge.py
+++ vidmerge.py
@@ -1,9 +1,21 @@
 import ffmpeg
 import os
 
-CLIP_DIRECTORY = "./clips"
+CLIP_DIRECTORY = "./clips/"
+CLIP_FILES = os.listdir(CLIP_DIRECTORY)
 
-#
+def open_files():
+    return [ffmpeg.input(CLIP_DIRECTORY + file) for file in CLIP_FILES]
+
+#unfinished
 def sanitize_clips():
-    for file in os.listdir(CLIP_DIRECTORY):
-        print(file)
+    for file in CLIP_FILES:
+        (
+            ffmpeg.input(os.path.join(CLIP_DIRECTORY, file))
+            .filter("s")
+        )
+
+def combine_clips():
+    va_files = [item for sublist in map(lambda f: [f.video, f.audio], open_files()) for item in sublist]
+    joined_video = ffmpeg.concat(*va_files, v=1, a=1).node
+    ffmpeg.output(joined_video[0], joined_video[1], 'output.mp4').run()

87dc7595fe42bac2c5b4b62321d34ade54e59f82
miishin <miishing@gmail.com>
2023-01-20 00:07:58 -0500

    abstract the generator functions to take in any text so I can use these if I ever want them some other time

--- title_card_generator.py
+++ title_card_generator.py
@@ -8,12 +8,10 @@ def generate_blank_title_card(width, height):
     img = Image.new("RGB", (width, height), 0)
     return (img, ImageDraw.Draw(img))
 
-def generate_eva_card(width, height, username):
+def generate_eva_card(width, height, lines):
     image, draw = generate_blank_title_card(width, height)
-    lines = ["YOUR", "ANILIST", "FAVORITES", "USER:", username.upper()]
     font_sizes = [height // 6, height // 6, height // 5, height // 10, height // 8]
     fonts = [ImageFont.truetype(font=EVA_FONT, size=s) for s in font_sizes]
-    boundings = [fonts[n].getbbox(lines[n]) for n in range(len(lines))]
 
     x = y = font_sizes[0] // 2
     for n in range(len(lines)):
@@ -21,17 +19,20 @@ def generate_eva_card(width, height, username):
         draw.text((x, y), lines[n], fill="white", font=fonts[n])
         y += font_sizes[n]
     image.save("./eva.png")
-    print("done")
 
-def generate_bebop_card(width, height, username):
+def generate_bebop_card(width, height, text):
     image, draw = generate_blank_title_card(width, height)
-    text = "%s HAS A GOOD EYE..." % username.upper()
     font_size = height // 14
     font = ImageFont.truetype(font=BEBOP_FONT, size=font_size)
     bounding = font.getbbox(text)
     draw.text((width - bounding[2] - bounding[3], height - 3.5 * bounding[3]), text, fill='white', font=font)
     image.save("bebop.png")
 
+def generate_bebop_card_temp(width, height, username):
+    generate_bebop_card(width, height, "%s HAS A GOOD EYE..." % username.upper())
 
-#generate_bebop_card(*DEFAULT_DIMENSIONS, "miishin")
-generate_eva_card(*DEFAULT_DIMENSIONS, "miishin")
+def generate_eva_card_temp(width, height, username):
+    lines = ["YOUR", "ANILIST", "FAVORITES", "USER:", username.upper()]
+    generate_eva_card(width, height, lines)
+#generate_bebop_card_temp(*DEFAULT_DIMENSIONS, "miishin")
+generate_eva_card_temp(*DEFAULT_DIMENSIONS, "miishin")

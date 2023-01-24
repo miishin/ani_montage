4f98b54f5c535ca2daf63cbb3852b136e76bd6cc
miishin <miishing@gmail.com>
2023-01-23 23:31:47 -0500

    add function to clear clips/

--- clip.py
+++ clip.py
@@ -1,6 +1,9 @@
 import anilist, sakugabooru
 import requests
 from sys import argv
+from os import scandir, remove
+
+CLIP_DIRECTORY = './clips/'
 
 def get_popular_clips(usr):
     shows = anilist.get_user_favorites(usr)
@@ -11,11 +14,14 @@ def get_popular_clips(usr):
             mp4s.append(mp4link)
     return mp4s
 
+# downloads the mp4 from each link to clips/
 def download_clips(links):
     for link in links:
         response = requests.get(link[1])
-        with open(f"./clips/{link[0]}.mp4", "wb") as f:
+        with open(f".{CLIP_DIRECTORY}{link[0]}.mp4", "wb") as f:
             f.write(response.content)
 
-x = get_popular_clips(argv[1])
-download_clips(x)
+# delete all files in clips/
+def clear_clips():
+    for file in scandir(CLIP_DIRECTORY):
+        remove(file.path)

6201a6803ac27e7021eedcb5219677b02ea177ee
miishin <miishing@gmail.com>
2023-01-23 23:33:49 -0500

    comments

--- clip.py
+++ clip.py
@@ -5,6 +5,8 @@ from os import scandir, remove
 
 CLIP_DIRECTORY = './clips/'
 
+# Given an Anilist username, returns a list of links to 
+# Sakugabooru mp4 files corresponding to that user's favorite Anime
 def get_popular_clips(usr):
     shows = anilist.get_user_favorites(usr)
     mp4s = []
@@ -14,14 +16,14 @@ def get_popular_clips(usr):
             mp4s.append(mp4link)
     return mp4s
 
-# downloads the mp4 from each link to clips/
+# Downloads the mp4 from each link to clips/ directory
 def download_clips(links):
     for link in links:
         response = requests.get(link[1])
         with open(f".{CLIP_DIRECTORY}{link[0]}.mp4", "wb") as f:
             f.write(response.content)
 
-# delete all files in clips/
+# Delete all files in clips/
 def clear_clips():
     for file in scandir(CLIP_DIRECTORY):
         remove(file.path)

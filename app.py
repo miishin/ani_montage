23c3cc16fa647e2515bf3b3ee79c33c019629bbd
miishin <miishing@gmail.com>
2023-01-23 23:10:36 -0500

    app file that will run everything I want

---
+++ app.py
@@ -0,0 +1,17 @@
+import clip, vidmerge
+import title_card_generator as tcg
+from sys import argv
+
+def main():
+    print("Grabbing video links")
+    mp4links = clip.get_popular_clips(argv[1])
+    print("Downloading videos!")
+    clip.download_clips(mp4links)
+    print("Generating title card")
+    tcg.main(argv[1], argv[2])
+    print("Generating reel")
+    vidmerge.combine_clips()
+    print("Done!")
+
+if __name__ == '__main__':
+    main()
